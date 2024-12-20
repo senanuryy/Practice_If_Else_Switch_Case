397e16101cdb6f6ef5da3417c3f5dd2548f784b5
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2024-12-20 12:15:03 +0300

    Add project files.

---
+++ Practice_If_Else_Switch_Case/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Practice_If_Else_Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If/Else");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Ruya Manavina Hos Geldiniz!");
+
+            Console.WriteLine("Elma = 2 TL");
+            Console.WriteLine("Armut = 3 TL");
+            Console.WriteLine("Cilek = 2 TL");
+            Console.WriteLine("Muz = 3 TL");
+            Console.WriteLine("Diger butun meyveler = 4 TL");
+
+            Console.Write("\nHangi meyveyi satin almak istersiniz? (Elma/Armut/Cilek/Muz/Diger): ");
+            string meyve = Console.ReadLine().ToLower();
+
+            if (meyve == "elma")
+            {
+                Console.WriteLine("Sectiginiz meyvenin fiyati: 2 TL");
+            }
+            else if (meyve == "armut")
+            {
+                Console.WriteLine("Sectiginiz meyvenin fiyati: 3 TL");
+            }
+            else if (meyve == "cilek")
+            {
+                Console.WriteLine("Sectiginiz meyvenin fiyati: 2 TL");
+            }
+            else if (meyve == "muz")
+            {
+                Console.WriteLine("Sectiginiz meyvenin fiyati: 3 TL");
+            }
+            else
+            {
+                Console.WriteLine("Sectiginiz meyvenin fiyati: 4 TL");
+            }
+            Console.WriteLine("\n\nSwitch/Case");
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("Ruya Manavina Hos Geldiniz!");
+
+            Console.WriteLine("Elma = 2 TL");
+            Console.WriteLine("Armut = 3 TL");
+            Console.WriteLine("Cilek = 2 TL");
+            Console.WriteLine("Muz = 3 TL");
+            Console.WriteLine("Diger butun meyveler = 4 TL");
+
+            Console.Write("Hangi meyveyi satin almak istersiniz? (Elma/Armut/Cilek/Muz/Diger: ");
+            string meyve2 = Console.ReadLine().ToLower();
+
+            switch (meyve2)
+            {
+                case "elma":
+                    Console.WriteLine("Sectiginiz meyvenin fiyati: 2 TL");
+                    break;
+                case "armut":
+                    Console.WriteLine("Sectiginiz meyvenin fiyati: 3 TL");
+                    break;
+                case "cilek":
+                    Console.WriteLine("Sectiginiz meyvenin fiyati: 2 TL");
+                    break;
+                case "muz":
+                    Console.WriteLine("Sectiginiz meyvenin fiyati: 3 TL");
+                    break;
+                default:
+                    Console.WriteLine("Sectiginiz meyvenin fiyati: 4 TL");
+                    break;
+
+            }
+
+
+        }
+
+    }
+
+}
